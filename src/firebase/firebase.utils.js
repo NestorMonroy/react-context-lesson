d2db11114e4da5e8106d4d17567e7e44b2a48b7d
NestorMonroy <nestor.monroy.90@gmail.com>
2020-09-29 18:10:32 -0500

    adding provider

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
-};
+  apiKey: "AIzaSyDY7h1aEqsiJVouyzTRhA8KQPdeymC2v5E",
+  authDomain: "crown-bf5bf.firebaseapp.com",
+  databaseURL: "https://crown-bf5bf.firebaseio.com",
+  projectId: "crown-bf5bf",
+  storageBucket: "crown-bf5bf.appspot.com",
+  messagingSenderId: "4633353727",
+  appId: "1:4633353727:web:2f00f8450451c33d2b9bca"
+}
 
 firebase.initializeApp(config);
 
